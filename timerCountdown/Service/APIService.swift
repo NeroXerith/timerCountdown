4f7c3ba2a57a6e8ab64ce4d6afa53aaf6c9b1358
NeroXerith <astar8820@gmail.com>
2025-08-15 21:19:26 +0800

    feat: Added a simulated API

--- timerCountdown/Service/APIService.swift
+++ timerCountdown/Service/APIService.swift
@@ -8,9 +8,20 @@
 import Foundation
 import Combine
 
-protocol APIService {
-//    func fetchData() -> AnyPublisher<Data, Error> {
-//        
-//        return nil as! AnyPublisher<Data, Error>
-//    }
+class APIService {
+    // Simulate API to fetch remaining duration
+    func fetchRemainingDuration() -> AnyPublisher<Int, Never> {
+        // Simulate 10 seconds duration from backend (in milliseconds)
+        let durationMilliseconds = 10_000
+        return Just(durationMilliseconds).eraseToAnyPublisher()
+    }
+
+    // Simulate API validation on button press
+    func validateButtonAction() -> AnyPublisher<Bool, Error> {
+        // Simulate success after 1s delay
+        return Just(true)
+            .delay(for: .seconds(1), scheduler: RunLoop.main)
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+    }
 }
