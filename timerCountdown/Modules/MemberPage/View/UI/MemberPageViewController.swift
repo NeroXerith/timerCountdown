c2e74160cdfac7f7d6708578d2c2d142688dc71a
NeroXerith <astar8820@gmail.com>
2025-08-17 21:08:18 +0800

    feat: Added UI to member page and button to navigate to home

--- timerCountdown/Modules/MemberPage/View/UI/MemberPageViewController.swift
+++ timerCountdown/Modules/MemberPage/View/UI/MemberPageViewController.swift
@@ -12,18 +12,14 @@ class MemberPageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Member Page"
-        // Do any additional setup after loading the view.
     }
 
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func homeButtonTapped(_ sender: Any) {
+        let vc = storyboard?.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
+        
+        self.navigationController?.pushViewController(vc, animated: true)
     }
-    */
+    
 
 }
